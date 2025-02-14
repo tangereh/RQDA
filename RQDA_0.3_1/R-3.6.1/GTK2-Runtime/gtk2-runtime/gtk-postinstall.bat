f9e30366c68190b43cc1216f746452f46acfa9b0
tangereh <82828474+tangereh@users.noreply.github.com>
2025-02-14 09:37:21 -0500

    first commit for RQDA copy

---
+++ RQDA_0.3_1/R-3.6.1/GTK2-Runtime/gtk2-runtime/gtk-postinstall.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\CAQDA\GTK2-Runtime\bin\pango-querymodules.exe" > "C:\CAQDA\GTK2-Runtime\etc\pango\pango.modules"
+rem "C:\CAQDA\GTK2-Runtime\bin\gdk-pixbuf-query-loaders.exe" > "C:\CAQDA\GTK2-Runtime\etc\gtk-2.0\gdk-pixbuf.loaders"
+"C:\CAQDA\GTK2-Runtime\bin\gtk-query-immodules-2.0.exe" > "C:\CAQDA\GTK2-Runtime\etc\gtk-2.0\gtk.immodules"
+rem "C:\CAQDA\GTK2-Runtime\bin\gtk-update-icon-cache.exe"
---
+++ RQDA_0.3_1/RQDA.bat
@@ -0,0 +1,1 @@
+start ./R-3.6.1/bin/x64/Rgui.exe
